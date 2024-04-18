ee9c9c366ef4e6a369e76ed8f80c4f8418d3044f
Eazed <eazed_yu@outlook.com>
2024-04-18 20:43:19 +0800

    苍穹外卖 阿里云Oss配置

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,27 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 通用接口
+ */
+@Slf4j
+@Api(tags = "通用接口")
+@RestController
+@RequestMapping("/admin/common")
+public class CommonController {
+
+    @ApiOperation("文件上传")
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传");
+        return null;
+    }
+}
