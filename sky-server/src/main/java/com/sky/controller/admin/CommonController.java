586b85fdc578fa532929ee4bf7943f3430ac90ec
Eazed <eazed_yu@outlook.com>
2024-04-18 21:01:24 +0800

    苍穹外卖 阿里云Oss文件上传

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,6 +1,8 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 通用接口
@@ -18,10 +23,30 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/admin/common")
 public class CommonController {
 
+    private final AliOssUtil aliOssUtil;
+
+    public CommonController(AliOssUtil aliOssUtil) {
+        this.aliOssUtil = aliOssUtil;
+    }
+
     @ApiOperation("文件上传")
     @PostMapping("/upload")
     public Result<String> upload(MultipartFile file) {
         log.info("文件上传");
-        return null;
+
+        try {
+            String originalFilename = file.getOriginalFilename();
+
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+            String objectName = UUID.randomUUID().toString() + extension;
+
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
