c2819ae340bd671e09ee4c22d3c5c866bd99790d
Eazed <75923878+Eazed-Yu@users.noreply.github.com>
2024-06-02 20:44:41 +0800

    苍穹外卖 数据查询（销量前十） (#11)

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,10 +1,12 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -44,4 +46,6 @@ public interface OrderMapper {
     Double sumByMap(Map map);
 
     Integer countByMap(Map map);
+    
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.sky.service;
 
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,9 @@ public interface ReportService {
     TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
 
     UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
-
-
+    
+    
     OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+    
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
 }
